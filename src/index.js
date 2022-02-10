10359506b473e45b676565401d7c6180683e119d
dishantaggarwal <dishantaggarwal24@gmail.com>
2022-02-10 15:33:47 +0530

    adding files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { AppProvider } from './context'
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.register();
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
